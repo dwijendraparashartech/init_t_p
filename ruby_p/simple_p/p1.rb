5364a7625dad38fcdf96dca003c206e7a92bf3f9
Arvind Kushwah <arvind.kushwah@avantinfotechnologies.com>
2023-01-12 12:17:00 +0530

    add first programs

---
+++ ruby_p/simple_p/p1.rb
@@ -0,0 +1,12 @@
+=begin
+Write a program to check the year is leap year or not 
+=end
+puts "enter the year you want to check that a leap year or not"
+yr = gets.chomp.to_i
+if yr%400 == 0
+	puts " #{yr} enter the year is leap year"
+elsif yr%4 ==0 &&  yr%100 != 0 
+	puts "#{yr} enter the year is leap year"
+else 
+	puts "#{yr} the year is not the leap year"
+end
---
+++ ruby_p/simple_p/p2.rb
@@ -0,0 +1,15 @@
+=begin
+ruby program to print the pwoer of a number 
+=end
+
+def pow(a,b)
+
+end 
+
+puts "Enter Base:- "
+base = gets.chomp.to_i
+
+puts "Enter the Exponent:- "
+exponent = gets.chomp.to_i
+
+puts "The power is #{pow(base,expo)}"
