de4bbc55f044ca59a7667f7f1d3a9d87259d7756
Arvind Kushwah <arvind.kushwah@avantinfotechnologies.com>
2023-01-13 16:51:45 +0530

    Add commit

---
+++ ruby_p/other_p/timing_programs.rb
@@ -0,0 +1,19 @@
+date_time = Time.new();
+currentDateTime = date_time.inspect();
+puts "Current Date Time: " + currentDateTime ;
+
+.........
+
+date_time = Time.now();
+currentDateTime = date_time.inspect();
+puts "Current Date Time: " + currentDateTime ;
+
+.........
+
+
+date_time = Time.now();
+currentDateTime = date_time.inspect();
+print "Current Date Time: " , currentDateTime ;
+
+.........
+
