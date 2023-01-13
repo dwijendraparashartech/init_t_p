9b9f851fc399a5944cc2717836822eec021a1d36
Arvind Kushwah <arvind.kushwah@avantinfotechnologies.com>
2023-01-13 17:11:09 +0530

    Project Description

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_13_100543) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_13_112410) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -22,6 +28,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_13_100543) do
     t.string "commenter"
     t.text "body"
     t.integer "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.float "price"
+    t.integer "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
