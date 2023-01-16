e5734df45f1013620d89bc4ab9a420e8ad928de8
Arvind Kushwah <arvind.kushwah@avantinfotechnologies.com>
2023-01-16 16:04:11 +0530

    Add some of the gems

--- ruby_p/simple_p/p2.rb
+++ ruby_p/simple_p/p2.rb
@@ -1,17 +1,36 @@
-=begin 
-To write a porgram to get the pwoer of a  number 
+# =begin 
+# To write the power of a number is 
+# =end
+
+# def pow(a,b)
+# 	power = 1
+# 	for i in 1..b  
+# 		power = power*a
+# 	end
+# 		return power 
+# end 
+
+# puts "enter the base of the number"
+# base = gets.chomp.to_i
+# puts "enter the expo of the number"
+# expo = gets.chomp.to_i
+# puts "the power of the number is #{pow(base,expo)}"
+
+
+=begin  
+To get the power of a number 
 =end 
 
 def pow(a,b)
-	power =1
-	for i in 1..b
-		power =power*a 
+	power = 1
+	for i in 1..b 
+		power = power*a  
 	end 
 	return power
 end
-
-puts "Enter the base of the number"
+puts "enter the base of the number:-"
 base = gets.chomp.to_i
-puts "Enter the exponent of the number"
-expo = gets.chomp.to_i 
-puts "The power of the number is #{pow(base,expo)}"
+puts "enter the power of a number:- "
+expo = gets.chomp.to_i
+the "power of the number is #{pow(a,b)}"
+
